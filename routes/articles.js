e6119ea0d69eb4cb33c425ede987aae7049af123
acquahsamuel <iamsamuelacquah@gmail.com>
2021-10-22 14:34:49 +0000

    [WIP] +> routes : articles,auth, comments, users

---
+++ routes/articles.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const Article = require("../models/Article");
+const { protect, authorize } = require("../middleware/auth");
+const advancedResults = require("../middleware/advancedResults");
+
+const {
+    getArticles,
+    getArticle,
+    createArticle,
+    updateArticle,
+    deleteArticle,
+} = require("../controllers/articles");
+const router = express.Router();
+
+
+// const router = express.Router({
+//     mergeParams: true
+// });
+
+
+router
+    .route("/")
+    .get(advancedResults(Article), getArticles)
+    .post(createArticle);
+
+router.route("/:id")
+    .get(getArticle)
+    .put(updateArticle)
+    .delete(deleteArticle);
+
+module.exports = router;
