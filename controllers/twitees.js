a0d138abf7e348c6b20298dd60066a92e0b8778e
acquahsamuel <iamsamuelacquah@gmail.com>
2021-10-23 07:41:14 +0000

    [WIP] +> twitees : comments fixes

---
+++ controllers/twitees.js
@@ -0,0 +1,80 @@
+const Twitee = require('../models/Twitee')
+const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
+
+// @desc      Get all twitee
+// @route     GET /api/v1/auth/users
+// @access    Private/Admin
+exports.getTwitees = asyncHandler(async (req, res, next) => {
+  await res.status(200).json(res.advancedResults)
+})
+
+// @desc      Create twitee
+// @route     POST /api/v1/auth/users
+// @access    Private/Admin
+exports.createTwitee = asyncHandler(async (req, res, next) => {
+  const twitee = await Twitee.create(req.body)
+  res.status(201).json({
+    success: true,
+    data: twitee,
+  });
+});
+
+// @desc      Get single twitee
+// @route     GET /api/v1/auth/users/:id
+// @access    Private/Admin
+exports.getTwitee = asyncHandler(async (req, res, next) => {
+  const twitee = await Twitee.findById(req.params.id)
+  if (!twitee) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404),
+    )
+  }
+
+  res.status(200).json({
+    success: true,
+    data: twitee,
+  })
+})
+
+// @desc      Update twitee
+// @route     PUT /api/v1/auth/users/:id
+// @access    Private/Admin
+exports.updateTwitee = asyncHandler(async (req, res, next) => {
+  const twitee = await Twitee.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  // if the twitee with the id is not found return error
+  if (!twitee) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404),
+    )
+  }
+
+  res.status(200).json({
+    success: true,
+    data: twitee,
+  })
+})
+
+
+// @desc      Delete twitee
+// @route     DELETE /api/v1/auth/users/:id
+// @access    Private/Admin
+exports.deleteTwitee = asyncHandler(async (req, res, next) => {
+  const twitee = await Twitee.findByIdAndDelete(req.params.id)
+
+  // if the twitee with the id is not found return error
+  if (!twitee) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404),
+    )
+  }
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  })
+})
