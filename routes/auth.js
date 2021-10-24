fff5a4174587a240bf0f493abddc32d9d55817d5
acquahsamuel <iamsamuelacquah@gmail.com>
2021-10-24 20:58:38 +0000

    [WIP] +> routes : modified getMe routes protect

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
-router.get('/me', protect, getMe);
+router.get('/me',getMe);
 
 
 
